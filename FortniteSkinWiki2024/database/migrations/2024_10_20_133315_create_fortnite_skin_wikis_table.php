5ceafecd6e455e62e1b1e98e92e83004838c9002
Tom McCarthy <n00230551@iadt.ie>
2024-10-20 18:38:46 +0100

    creating more data for the database

---
+++ FortniteSkinWiki2024/database/migrations/2024_10_20_133315_create_fortnite_skin_wikis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fortnite_skin_wikis', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('vbuck_price');
+            $table->integer('rarity');
+            $table->string('season');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fortnite_skin_wikis');
+    }
+};
